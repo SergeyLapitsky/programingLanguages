8a5154f4a5a4b925a8a0e4561e4e8bfdfb1c7dd6
Sergey <sergeylapitsky@yandex.ru>
2024-04-11 15:20:41 +0300

    add seminar:task3

---
+++ Sem2/task3/Program.cs
@@ -0,0 +1,27 @@
+﻿//Напишите программу, которая будет принимать на
+//вход два числа и выводить, является ли первое число
+//кратным второму. Если первое число некратно
+//второму, то программа выводит остаток от деления.
+//Примеры
+//14, 5 => нет, 4
+//16, 8 => да
+//4, 3 => нет, 1
+
+void CheckIfDevides(int num1, int num2)
+{
+    if(num1 % num2 == 0)
+    {
+        System.Console.WriteLine($"{num1}, {num2} -> yes");
+    }
+    else
+    {
+        System.Console.WriteLine($"{num1}, {num2} -> no, {num1 % num2}");
+    }
+}
+
+Console.Write("Input first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+CheckIfDevides(num1, num2);
