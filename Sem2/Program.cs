771caec2dc66ad9289fa8a4456ef687225c504e2
Sergey <sergeylapitsky@yandex.ru>
2024-04-11 11:12:04 +0300

    add seminar2:task1

--- Sem2/Program.cs
+++ Sem2/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("Hello, World!");
+﻿void NumToQuad (int num)
+{
+    System.Console.WriteLine(num * num);
+}
+
+int NumToQuadInt(int num)
+{
+    return num * num;
+}
+
+System.Console.WriteLine("Input number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+//NumToQuad(a);
+//int result = NumToQuadInt(a);
+//System.Console.WriteLine(result);
+System.Console.WriteLine(NumToQuadInt(a));
---
+++ Sem2/task1/Program.cs
@@ -0,0 +1,32 @@
+﻿//Напишите программу, которая принимает на
+//вход трёхзначное число и удаляет вторую
+//цифру этого числа.
+//Примеры
+//a = 256 => 26
+//a = 891 => 81
+
+int DeleteSecondDigit(int num)
+{
+    if(num >= 100 && num <= 999)
+    {
+        // для единиц
+        int ed = num % 10;
+        // для сотен
+        int sot = num / 100;
+        // первый способ 
+        //int result = sot * 10 + ed;
+        //return result;
+        //укороченный метод 
+        return sot * 10 + ed;
+    }
+    else 
+    {
+        System.Console.WriteLine("You input not three-digit number");
+        return 0;
+    }
+}
+
+System.Console.Write("Input three-digit number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine(DeleteSecondDigit(num));
