09e04ba880ede85b55155e6d348e0f64eccc810e
Sergey <sergeylapitsky@yandex.ru>
2024-04-11 15:51:23 +0300

    add practicalexercises:task2

---
+++ PracticalExercises/task2/Program.cs
@@ -0,0 +1,35 @@
+﻿void DefinitionOfCoordinateQuarter (int x, int y)
+{
+    if(x != 0 && y != 0 )
+    {
+        if(x < 0)
+        {
+            if(y < 0)
+            {
+                System.Console.WriteLine("Number is in the Third quarter");
+            }
+            else
+            {
+                System.Console.WriteLine("Number is in the Second quarter");
+            }
+        }
+        else 
+        {
+           if(y < 0)
+            {
+                System.Console.WriteLine("Number is in the Fourth quarter");
+            }
+            else
+            {
+                System.Console.WriteLine("Number is in the First quarter");
+            } 
+        }
+    }
+}
+
+Console.Write("Input x: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+DefinitionOfCoordinateQuarter(x, y);
