680185c38e018aaace01aca866155f5ea48eec77
Sergey <sergeylapitsky@yandex.ru>
2024-04-11 16:21:38 +0300

    add practiceexercises:task3

--- PracticalExercises/task2/Program.cs
+++ PracticalExercises/task2/Program.cs
@@ -1,4 +1,7 @@
-﻿void DefinitionOfCoordinateQuarter (int x, int y)
+﻿//Задача 2: Напишите программу, которая принимает на вход координаты точки (X и Y),
+//причём X ≠ 0 и Y ≠ 0 и выдаёт номер координатной четверти плоскости, в которой находится эта точка.
+
+void DefinitionOfCoordinateQuarter (int x, int y)
 {
     if(x != 0 && y != 0 )
     {
---
+++ PracticalExercises/task3/Program.cs
@@ -0,0 +1,31 @@
+﻿//Задача 3: Напишите программу, которая принимает на вход целое
+//число из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+int FindLargestDigit(int num)
+{
+    if(num >= 10 && num <= 99)
+    {
+        // единицы
+        int ed = num % 10;
+        // десятки
+        int dec = num / 10 % 10;
+        if(ed > dec)
+        {
+            return ed;
+        }
+        else
+        {
+            return dec;
+        }
+    }
+    else 
+    {
+        System.Console.WriteLine("You input number is out of range");
+        return 0;
+    }
+}
+
+System.Console.Write("Input number in the range [10. 99]: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine(FindLargestDigit(num));
