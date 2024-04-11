dba68f1cc0837e2305ad2b4905f0815f38ba9d0e
Sergey <sergeylapitsky@yandex.ru>
2024-04-11 15:38:28 +0300

    add practical exercises

---
+++ PracticalExercises/task1/Program.cs
@@ -0,0 +1,16 @@
+﻿void CheckIfDevides(int num)
+{
+    if(num % 7 == 0 && num % 23 == 0)
+    {
+        System.Console.WriteLine("Number multiple 7 and 23");
+    }
+    else 
+    {
+        System.Console.WriteLine("Number not multiple 7 and 23");
+    }
+}
+
+Console.Write("Input number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+CheckIfDevides(num);
